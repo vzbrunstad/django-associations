f30d6a98f5be4c80e9f7912b97456fb205a999a5
Mic-J-Lee <mleenavy@gmail.com>
2019-10-08 14:26:24 -0500

    change code style

--- amazon/store/models.py
+++ amazon/store/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-
+    pass
 
 class Shop(models.Model):
-
+    pass
 
 class Product(models.Model):
-
+    pass
 
 class Review(models.Model):
-
-
+    pass
--- medium/blog/models.py
+++ medium/blog/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    pass
 
 class Post(models.Model):
+    pass
 
 class Comment(models.Model):
+    pass
   
