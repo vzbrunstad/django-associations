0ff88df539d4dfd6c3a73372143d8e51debc364d
vzbrunstad <vzbrunstad@msn.com>
2021-06-30 16:30:50 -0500

    Vince B Django Associations release 0

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Actor(models.Model):
+    pass
+
+class Movie(models.Model):
+    actors = models.ManyToManyField(Actor,through='Role',related_name='movies')
+    
+class Role(models.Model):
+    actor= models.ForeignKey(Actor, on_delete=models.CASCADE,related_name='roles')
+    movie= models.ForeignKey(Movie, on_delete=models.CASCADE,related_name='roles')
